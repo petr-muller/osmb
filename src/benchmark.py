c27c41a65cd517495d3c2dda885284b585822f7c
Petr Muller <afri@afri.cz>
2010-04-17 23:51:28 +0200

    probe launching integration

--- src/benchmark.py
+++ src/benchmark.py
@@ -5,6 +5,7 @@ import sys
 import tempfile
 import subprocess
 import os
+import osmb
 
 VERBOSE=False
 HELPER_PATH='helpers'
@@ -28,7 +29,32 @@ def simple_checked_command(message, command):
     print "[ PASS ]"
   else:
     print "[ FAIL ]"
-    abort("Failure: %s" % message)
+    abort(message)
+    
+def simple_checked_result(message, result):
+  print message.ljust(50),
+  
+  if result:
+    print "[ PASS ]"
+  else:
+    print "[ FAIL ]"
+    abort(message)
+
+def probe(name, options):
+  probe = osmb.probe.Probe()
+  simple_checked_result("Loading the probe", probe.loadProbe(name))
+
+  probe.setScenario(options.scenario)
+  probe.setBSize(options.bsize)
+  probe.setSSize(options.ssize)
+  probe.setMalloc(options.malloc)
+  probe.setFree(options.free)
+  probe.setAllocator(options.allocator)
+  probe.setPythonPath(os.path.join(os.getcwd()))
+  
+  simple_checked_result("Probe validation", probe.validate())
+  simple_checked_result("Probe preparation", probe.prepare())
+  simple_checked_result("Probe run", probe.launch())
 
 def command_test(allocator, malloc, free):
   verbose("Testing allocator: %s" % allocator)
@@ -104,17 +130,25 @@ if __name__ == '__main__':
   parser.add_option('-a', '--allocator', dest='allocator',
                     help='Path to allocator to test. If not specified, '\
                          ' system library will be used',
-                    default=False, metavar='FILE')
+                    default=None, metavar='FILE')
   parser.add_option('-v', '--verbose', dest='verbose', default=False,
                     action="store_true", help="More verbose output")
   parser.add_option('-s', '--scenario', dest='scenario', metavar='FILE',
-                    help='Scenario file to use')
+                    help='Scenario file to use', default=None)
   parser.add_option('-m', '--malloc', dest='malloc', default='malloc',
                     help="Name of malloc function in a library",
                     metavar='MALLOC_FUNCTION')
   parser.add_option('-f', '--free', dest='free', default='free',
                     help="Name of free function in a library",
                     metavar='FREE_FUNCTION')
+  parser.add_option("-p", "--probe", dest="probe", default=None,
+                    help="Probe name", metavar="PROBE")
+  parser.add_option("-c", "--count", dest="ssize", default=1,
+                    help="Sample size (number of measurements)",
+                    metavar="NUMBER")
+  parser.add_option("-b", "--bsize", dest="bsize", default=1,
+                    help="Batch size (number of executions per measurements)",
+                    metavar="NUMBER")
   
   (options, args) = parser.parse_args()
   
@@ -129,5 +163,9 @@ if __name__ == '__main__':
   if command == "test":
     if not options.allocator:
       abort("Test command needs an allocator (-a option)")
-    
     command_test(options.allocator, options.malloc, options.free)
+
+  elif command == "probe":
+    if not options.probe:
+      abort("Probe command needs a probe (-p option)")
+    probe(options.probe, options)
