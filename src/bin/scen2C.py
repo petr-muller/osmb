00862a371f9167bf0312de5c17da8d863c499c1b
Petr Muller <pmuller@redhat.com>
2010-05-20 01:59:43 +0200

    added better error handling

--- src/bin/scen2C.py
+++ src/bin/scen2C.py
@@ -7,9 +7,12 @@ if __name__ == '__main__':
   fp = open(sys.argv[1], 'r')
   scen = osmb.scenario_parser.parseScenario(fp)
   fp.close()
-  
+
+  if not scen:
+    print >> sys.stderr, "Scenario parsing failed"
+
   scen.validate()
-  
+
   if scen.isValid():
     print scen.translateToC()
   else:
