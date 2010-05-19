d417c3ad4f65ee94230a59d98209e731cf157d92
Petr Muller <pmuller@redhat.com>
2010-05-20 01:58:08 +0200

    updates to the text

--- DIP/done.sh
+++ DIP/done.sh
@@ -6,4 +6,5 @@ CHARS=`wc -c stripped.txt | cut -d ' ' -f 1 `
 NPAGES=$(($CHARS/1800))
 echo "Znaku:      $CHARS"
 echo "Normostran: $NPAGES"
+echo "2nd normo:  $((`wc -c obsah.tex | cut -f 1 -d ' '` / 2000 ))"
 echo "Goal:       100"
