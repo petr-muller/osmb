764ad5a2994c7b8318e817c9e715249362c3f7be
Petr Muller <pmuller@redhat.com>
2010-05-20 02:13:14 +0200

    use normal statistics

--- src/probes/totaltime/evaluate.py
+++ src/probes/totaltime/evaluate.py
@@ -10,13 +10,13 @@ realMetric    = osmb.metric.Metric("Real time", dset.getList("REAL"))
 systemMetric  = osmb.metric.Metric("System time", dset.getList("SYSTEM"))
 totalMetric   = osmb.metric.Metric("Total CPU time", dset.getSumList(["USER", "SYSTEM"]))
 print "=" * 80
-print userMetric.statDetails()
-print realMetric.statDetails()
-print systemMetric.statDetails()
-print totalMetric.statDetails()
+print userMetric.normalStatDetails()
+print realMetric.normalStatDetails()
+print systemMetric.normalStatDetails()
+print totalMetric.normalStatDetails()
 
-fp = open(filename, 'r')
-for line in fp.readlines():
-  print line.strip()
-fp.close()
+#fp = open(filename, 'r')
+#for line in fp.readlines():
+#  print line.strip()
+#fp.close()
 print "=" * 80
